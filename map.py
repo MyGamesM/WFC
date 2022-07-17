1406c74160f3a635efd95fa271c4a336f817e76d
MyGamesM <milanmilan.mm55@gmail.com>
2022-07-17 18:08:08 +0200

    fixed wierd characters

--- map.py
+++ map.py
@@ -5,19 +5,18 @@ class Map:
 		self.map_width = width
 		self.map_height = height
 		self.generate_map()
-		
+
 	def generate_map(self) -> None:
 		self.map = [[None for _ in range(self.map_width)] for _ in range(self.map_height)]
 
 	def get(self) -> list:
 		return self.map
-	
 	def get_size(self) -> tuple:
 		return self.map_width - 1, self.map_height - 1
-		
+
 	def get_tile(self, x: int, y: int) -> Tile or None:
 		return self.map[y][x]
-		
-	def set_tile(self, x: int, y: int, tile: Tile, override: bool):
+
+    def set_tile(self, x: int, y: int, tile: Tile, override: bool):
 		if self.map[y][x] is None or override is True:
 			self.map[y][x] = tile
--- tile.py
+++ tile.py
@@ -5,38 +5,27 @@ class Tile():
 		self.entropy = 0
 		self.dirs = []
 		self.collapsed = False
-		
 	def get_value(self) -> int:
 		return self.value
-		
 	def set_value(self, val: int) -> None:
 		if type(val) is not int:
 			raise ValueError(f"val is {type(x)} not int")
 		if val != -1:
 			self.collapsed = True
-				
 		self.value = val
-		
 	def add_rule(self, rule: tuple) -> None:
 		self.rules.append(rule)
-		
 	def get_rules(self) -> list:
 		return self.rules
-		
 	def set_entrpy(self, e: tuple) -> None:
 		self.entropy = e
-		
 	def get_entropy(self) -> int:
 		return self.entropy
-		
 	def add_dir(self, dir: tuple) -> None: # maybe extend only
 		self.dirs.append(dir)
-		
 	def get_dirs(self) -> list:
 		return self.dirs
-		
 	def set_col(self, val: bool) -> bool:
 		self.collapsed = val
-		
 	def get_col(self) -> bool:
 		return self.collapsed
